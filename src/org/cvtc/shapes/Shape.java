b1ae2137ee8b55c0839e8325eb6a6ed1d927e9af
danya <danya23-as@hotmail.com>
2018-09-19 17:53:16 -0500

    Shapes 4

--- src/org/cvtc/shapes/Shape.java
+++
@@ -1,9 +0,0 @@
-package org.cvtc.shapes;
-
-public abstract class Shape {
-	//Abstract classes cannot be instantiated, but they can be sub classed.
-	//In other words, a class that is declared with abstract keyword
-	public abstract float surfaceArea();
-	public abstract float volume();
-	public abstract void render();
-}
